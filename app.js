7dc2c769b803516e931b6f19c3fa9f8ebda56dfc
Anuj Tomar <ankuto@gmail.com>
2015-04-27 14:47:07 -0700

    added template

--- app.js
+++ app.js
@@ -2,9 +2,21 @@ angular.module('app', [])
 
 .controller('MadController', ['$scope', function($scope){
   $scope.words = [];
+  $scope.template = ["$scope.words[i] went for a walk in the park.", " {{userInput}} found a {{userInput}}. {{userInput}} decided to take it home."]
 
   $scope.addWord = function(word){
     $scope.words.push(word);
     $scope.userInput = '';
   }
+
+  $scope.getTemplate = function(){
+    var index = Math.floor(Math.random()*$scope.template.length);
+    return $scope.template;
+  }
+  
+  $scope.getStory = function(){
+    return $scope.story.join("");
+  }
+
+
 }])

2954bcaf6d5265df5a95ce7f15337853b769f369
Anuj Tomar <ankuto@gmail.com>
2015-04-27 15:23:02 -0700

    UGH

--- app.js
+++ app.js
@@ -2,21 +2,32 @@ angular.module('app', [])
 
 .controller('MadController', ['$scope', function($scope){
   $scope.words = [];
-  $scope.template = ["$scope.words[i] went for a walk in the park.", " {{userInput}} found a {{userInput}}. {{userInput}} decided to take it home."]
+  $scope.template = ["went for a walk in the park.", "decided to take it home."]
+  $scope.story = [];
+  $scope.count = 0;
+  $scope.currentSentence = "";
 
   $scope.addWord = function(word){
     $scope.words.push(word);
     $scope.userInput = '';
   }
 
-  $scope.getTemplate = function(){
-    var index = Math.floor(Math.random()*$scope.template.length);
-    return $scope.template;
+  $scope.getSentence = function(){
+    //var index = Math.floor(Math.random()*$scope.template.length);
+    console.log("H!!!!!")
+    $scope.currentSentence = $scope.template[$scope.count];
+    $scope.story.push($scope.currentSentence);
+    console.log($scope.story)
+    $scope.count++;
   }
-  
+
   $scope.getStory = function(){
     return $scope.story.join("");
   }
+
+  $scope.$on('$viewContentLoaded', function(){
+    $scope.getSentence();
+  });
 
 
 }])

22d1a8e199bc7e20c90688764f767443879ad253
Anuj Tomar <ankuto@gmail.com>
2015-04-28 11:20:23 -0700

    pivoted to: CARDS FOR HUMANITY

--- public/client/app/app.js
+++ public/client/app/app.js
@@ -1,38 +1,56 @@
 angular.module('app', [])
 
 
-.controller('MadController', ['$scope', function($scope){
-  $scope.words = [];
-  $scope.template = ["% went for a walk in the park. ", "There was % on the ground.", "% decided to take it home.", "The % grew very large,", "So large that it was bigger than %"]
-  $scope.story = [];
+.controller('mainController', ['$scope', function($scope){
+  $scope.questions = ["What gets better with age? %", 
+  "I drink to forget %", 
+  "Next on ESPN2: The World Series of %", 
+  "A LifeTime Original Picture: %", 
+  "I'm not going to lie, I despise %. There, I said it.", 
+  "Cancel all my meetings. We've got a situation with % that requires my immediate attention.",
+  ];
+  $scope.answers = ["Lollipops", "Gummy bears", "Sunshine", "Rainbows", "The color yellow", "Golden retrievers", "The glowing realization that all of us are connected in an abstract yet liminal way", "The Beatles", "Warmth", "Honeysuckles", "Being well rested", "a postive perspective on life", "fiscal responsibility"];
+  $scope.sentencesSoFar = [];
   $scope.count = 0;
 
+  $scope.currentQuestion = "";
+  $scope.currentAnswers = [];
+
   $scope.userSentence;
+  $scope.userInput = "__________";
 
   $scope.addWord = function(word){
-    //$scope.words.push(word);
-    $scope.userSentence = $scope.getCurrentStorySentence().replace("%", $scope.userInput);
+    $scope.userSentence = $scope.currentQuestion.replace("%", word.toUpperCase());
     $scope.addSentence($scope.userSentence);
 
     $scope.count++;
-    $scope.userInput = '';
-
-    console.log($scope.getcurrentStorySentence, $scope.template[$scope.count])
-  }
-
-  $scope.getCurrentStorySentence = function(){
-    return $scope.template[$scope.count];
-
+    $scope.userInput = '__________';
   }
 
   $scope.addSentence = function(sentence){
     //var index = Math.floor(Math.random()*$scope.template.length);
-    $scope.story.push(sentence);
-   
+    $scope.sentencesSoFar.push(sentence);
+    console.log($scope.sentencesSoFar)
   }
 
-  $scope.getStory = function(){
-    return $scope.story.join("");
+
+  //==========INITIALIZE=============
+
+  $scope.getQuestion = function(){
+    var index = Math.floor(Math.random() * $scope.questions.length);
+
+    $scope.currentQuestion = $scope.questions[index];
+    return $scope.currentQuestion;
+
+  }
+
+  $scope.getAnswers = function(){
+    var index;
+    for(var i = 0; i < 5; i++){
+    var index = Math.floor(Math.random() * $scope.answers.length);
+     $scope.currentAnswers.push($scope.questions[index]);
+   }
+
   }
 
 /*
